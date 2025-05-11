85afe3c177e4e837217c573ef710f028e4e08559
Aryaman <109002742+Aryamann27@users.noreply.github.com>
2025-05-11 13:37:11 +0530

    "new"

---
+++ Codeforces-Questions/test.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+void solve()
+{
+    string str1 = "abc";
+    string str2 = "abc";
+
+    if (str1 < str2)
+    {
+        cout << "hi" << '\n';
+    }
+}
+
+int main()
+{
+    cin.tie(0)->sync_with_stdio(0);
+
+    int t;
+    cin >> t;
+    // t = 1;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
